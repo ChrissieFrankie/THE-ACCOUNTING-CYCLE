8aa21dc338c75c39c2ee279b25ceb13b658a29f0
ChrissieFrankie <cjfg00gl3@gmail.com>
2025-10-04 22:20:27 -0400

    create(main.cpp): list the accounting cycle

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(void)
+{
+  std::cout << "The Accounting Cycle (Wiley)" << std::endl;
+  std::cout << "Analyze business transactions" << std::endl;
+  std::cout << "Journalize the transactions" << std::endl;
+  std::cout << "Post to ledger accounts" << std::endl;
+  std::cout << "Prepare a trial balance" << std::endl;
+  std::cout << "Journalize and post adjusting entries" << std::endl;
+  std::cout << "Prepare an adjusted trial balance" << std::endl;
+  std::cout << "Prepare financial statements" << std::endl;
+  std::cout << "Journalize and post closing entries" << std::endl;
+  std::cout << "Prepare a postclosing trial balance" << std::endl;
+  return 0;
+
+}
